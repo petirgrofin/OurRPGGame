fb2a8ded1fa15420a1001e4d2c9d09803f536c83
petirgrofin <83374229+petirgrofin@users.noreply.github.com>
2021-05-02 13:27:15 -0600

    General changes to bleed_check(), almost finished.

--- RPGGameEnemies.py
+++ RPGGameEnemies.py
@@ -3,7 +3,7 @@ import random
 
 class Marauder:
     marauder_stats = {
-        "marauder_health": 500,
+        "marauder_health": 1000,
         "marauder_damage": 80,
         "marauder_speed": 40,
         "marauder_defense": 50,
