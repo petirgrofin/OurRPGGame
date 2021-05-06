129fc9aa7ee7adda5d017fc8c7ec76085d2c2f1d
petirgrofin <83374229+petirgrofin@users.noreply.github.com>
2021-05-05 19:01:16 -0600

    cambio pequeño a Enemies

--- RPGGameEnemies.py
+++ RPGGameEnemies.py
@@ -342,8 +342,8 @@ class RandomEnemy:
             if self.enemy_attack == AssassinWorm.cavern_enemies_assassin_worm_attack["assassin_worm_pounce"]:
                 self.enemy_stun_capable_attack = True
                 self.enemy_stun_chance = 100
-            if self.enemy_attack == AssassinWorm.cavern_enemies_assassin_worm_attack["assassin_worm_pistol_attack"]:
-                pass
+            else:
+                self.enemy_stun_capable_attack = False
 
 
 random_enemy = RandomEnemy()
